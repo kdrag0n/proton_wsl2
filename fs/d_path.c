0aee66bddad3c43132d21996617c5eeeaf7c15c4
Danny Lin <danny@kdrag0n.dev>
2019-07-03 18:56:42 -0700

    vfs: Eliminate dynamic memory allocation in getcwd() system call
    The overhead of dynamic memory allocation in this function is quite
    significant. Since the maximum path length is 4 KiB, we can use a stack
    allocation instead which speeds this call up by 5%.
    
    Before: 441 ns
    After:  419 ns
    
    Co-authored-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- fs/d_path.c
+++ fs/d_path.c
@@ -425,10 +425,7 @@ SYSCALL_DEFINE2(getcwd, char __user *, buf, unsigned long, size)
 {
 	int error;
 	struct path pwd, root;
-	char *page = __getname();
-
-	if (!page)
-		return -ENOMEM;
+	char page[PATH_MAX] __aligned(8);
 
 	rcu_read_lock();
 	get_fs_root_and_pwd_rcu(current->fs, &root, &pwd);
@@ -465,6 +462,5 @@ SYSCALL_DEFINE2(getcwd, char __user *, buf, unsigned long, size)
 	}
 
 out:
-	__putname(page);
 	return error;
 }
